216b297bbfc103b773a2f7c070edd43a53091904
Felipe Ferreira <sistema3@yescert.com.br>
2020-10-02 17:49:35 -0300

    test

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/produto', 'ProdutoController@index');
+
+// Laravel 8.x
+// Route::get('/produto', [ProdutoController::class, 'index']);
+
